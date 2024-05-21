e7ab08ee5c073657bb246ae34320ed93087dd3a7
karen <kdczzz@outlook.com>
2024-05-21 19:23:16 -0400

    Mensaje debe agregar al menos un producto para la venta

--- ventas/views.py
+++ ventas/views.py
@@ -1,37 +1,18 @@
-from django.shortcuts import render
-from django import forms
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group, User
-from django.core.mail import EmailMultiAlternatives
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.db.models import Avg, Count, Q
-from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.urls import reverse
-import json
-import pandas as pd
-import xlwt
-from datetime import datetime
-from django.http import HttpResponse
-import traceback
-import os
 from django.db import transaction
-from django.views.decorators.csrf import csrf_protect
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from registration.models import Profile
 from ventas.models import Prod_venta, Orden_venta, Venta_producto
-from administrator.views import validar_email, validar_rut, validar_string, validar_numero
-from inventario.models import Product
-
-# Create your views here.
 
 @login_required
 def ventas_main(request):
     profiles = Profile.objects.get(user_id=request.user.id)
     if profiles.group_id != 1 and profiles.group_id != 2:
-        messages.add_message(request, messages.INFO, 'Intenta ingresar a una area para la que no tiene permisos')
+        messages.add_message(request, messages.INFO, 'Intenta ingresar a una área para la que no tienes permisos')
         return redirect('check_group_main')
     template_name = 'ventas/ventas_main.html'
     return render(request, template_name, {'profiles': profiles})
@@ -41,7 +22,7 @@ def venta_save(request):
     if request.method == 'POST':
         cliente_name = request.POST.get('cliente_name')
         cliente_last_name = request.POST.get('cliente_last_name')
-        producto_venta = ['Completo Italiano', 'Completo Personalizado', 'Bebida']  # request.POST.getlist('producto_venta')
+        producto_venta = ['Completo Italiano', 'Completo Personalizado', 'Bebida']  # Esto debería obtenerse del formulario: request.POST.getlist('producto_venta')
         cantidad_venta = request.POST.getlist('cantidad_venta')
         cliente_venta = f"{cliente_name} {cliente_last_name}"
         
@@ -50,17 +31,18 @@ def venta_save(request):
             messages.error(request, 'Debes ingresar toda la información')
             return redirect('venta_crear')
         
+        # Comprobar que al menos un producto tenga una cantidad mayor a 0
+        cantidades_validas = [int(cant) if cant else 0 for cant in cantidad_venta]
+        if all(cant == 0 for cant in cantidades_validas):
+            messages.error(request, 'ERROR: Debe haber al menos un producto agregado cpara crear la venta')
+            return redirect('venta_crear')
+        
         try:
             with transaction.atomic():
                 orden_venta = Orden_venta.objects.create(cliente_venta=cliente_venta)
                 total_venta = 0
 
-                for prod, cant in zip(producto_venta, cantidad_venta):
-                    if cant == '':
-                        cant = 0  # Asignar 0 si la cantidad está vacía
-                    else:
-                        cant = int(cant)  # Convertir la cantidad a entero
-
+                for prod, cant in zip(producto_venta, cantidades_validas):
                     producto_instancia = Prod_venta.objects.get(nombre_producto=prod)
                     venta_producto = Venta_producto.objects.create(producto=producto_instancia, cantidad=cant, id_orden=orden_venta, precio_producto=producto_instancia.precio)
                     venta_producto.save()
@@ -122,49 +104,8 @@ def venta_crear(request):
         messages.add_message(request, messages.INFO, 'Intenta ingresar a un área para la que no tiene permisos')
         return redirect('check_group_main')
     
-    #clientes = Cliente.objects.all()
     productos = Prod_venta.objects.all()
     template_name = 'ventas/venta_crear.html'
-    
-    '''
-    if request.method == 'POST':
-        numero_orden = request.POST.get('numero_orden')
-        cliente_name = request.POST.get('cliente_name')
-        producto_orden = request.POST.get('producto_orden')
-        cantidad_orden = request.POST.get('cantidad_orden')
-        monto_orden = request.POST.get('monto_orden')
-        producto_orden = request.POST.get('producto_orden')
-        agregados_orden = request.POST.get('agregados_orden')
-        
-        # Mostrar los datos recibidos del formulario
-        print("Datos recibidos del formulario:")
-        print(f"Orden: {numero_orden}")
-        print(f"Cliente: {cliente_name}")
-        print(f"Producto: {producto_orden}")
-        print(f"Cantidad: {cantidad_orden}")
-        print(f"Monto: {monto_orden}")
-        print(f"Agregados: {agregados_orden}")
-
-        # Intentar crear la nueva orden de compra
-        try:
-            nueva_orden = Orden_venta.objects.create(
-                numero_orden=numero_orden,
-                cliente_name=cliente_name,
-                producto_orden=producto_orden,
-                cantidad_orden=cantidad_orden,
-                monto_orden=monto_orden,
-                agregados_orden=agregados_orden,
-                # Puedes agregar más campos aquí según sea necesario
-            )
-            # Guardar el ID de la nueva orden en la sesión
-            request.session['nueva_orden_id'] = nueva_orden.id
-            print("¡Venta creada con éxito!")
-        except Exception as e:
-            print(f"Error al crear la venta: {e}")
-
-        # Redirigir al usuario al listado de órdenes
-        return redirect('venta_list')
-    '''
     
     return render(request, template_name, {'profiles': profiles, 'productos': productos})
 
