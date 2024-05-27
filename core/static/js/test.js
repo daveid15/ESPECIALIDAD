8ea5cd623afe993d3f33a712cb8a40e18dd5228f
DiegoCastillo42 <diego.castillo3@cloud.uautonoma.cl>
2024-05-26 20:06:23 -0400

    Cambios insumos proveedores

--- core/static/js/test.js
+++ core/static/js/test.js
@@ -3,30 +3,13 @@ function test(){
     const div_insumo = document.createElement("div");
     const span = document.createElement("span");
     span.innerHTML = "proveedor_insumo";
-    select_insumo = document.createElement("select");
-    select_insumo.name = "proveedor_insumo";
-    select_insumo.id = "proveedor_insumo";
+    input_insumo = document.createElement("input");
+    input_insumo.name = "proveedor_insumo";
+    input_insumo.id = "proveedor_insumo";
 
-
-    option1 = document.createElement("option");
-    option1.setAttribute("value", "Palta");
-    option1Texto = document.createTextNode("Palta");
-    option1.appendChild(option1Texto);
- 
-    option2 = document.createElement("option");
-    option2.setAttribute("value", "Tomate");
-    option2Texto = document.createTextNode("Tomate");
-    option2.appendChild(option2Texto);
- 
-    option3 = document.createElement("option");
-    option3.setAttribute("value", "Pan");
-    option3Texto = document.createTextNode("Pan");
-    option3.appendChild(option3Texto);
- 
-    select_insumo.appendChild(option1);
-    select_insumo.appendChild(option2);
-    select_insumo.appendChild(option3);
-    document.getElementById("container").appendChild(select_insumo);
+    document.getElementById("container").appendChild(input_insumo);
 
     
 };
+
+    
